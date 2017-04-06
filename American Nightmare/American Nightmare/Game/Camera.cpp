9d1c8099466afd1adfd9a1bd142e743ee70527cd
Fredholm <te11c.simon.fredholm@gmail.com>
2017-04-06 13:47:43 +0200

    removed rotation

--- American Nightmare/American Nightmare/Game/Camera.cpp
+++ American Nightmare/American Nightmare/Game/Camera.cpp
@@ -7,11 +7,9 @@ Camera::Camera(const Camera& other) { }
 Camera::~Camera() { }
 
 void Camera::setPosition(glm::vec3 position) { this->position = position; }
-void Camera::setRotation(glm::vec3 rotation) { this->rotation = rotation; }
 
 glm::mat4 Camera::getViewMatrix() { return viewMatrix; }
 glm::vec3 Camera::getPosition() { return position; }
-glm::vec3 Camera::getRotation() { return rotation; }
 
 void Camera::buildViewMatrix()
 {
