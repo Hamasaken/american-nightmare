7e01b3937505f184e708cfee3147b188d34928cb
Fredholm <te11c.simon.fredholm@gmail.com>
2017-04-06 12:25:50 +0200

    Adding Input/Game/Program/OpenGL Files

---
+++ American Nightmare/American Nightmare/Input.cpp
@@ -0,0 +1,37 @@
+#include "Input.h"
+
+Input::Input() { }
+
+Input::Input(const Input&) { }
+
+Input::~Input() { }
+
+void Input::Start()
+{
+	// Initialize all the keys to being released and not pressed.
+	for (int i = 0; i < 256; i++)
+	{
+		m_key[i] = false;
+	}
+}
+
+bool Input::Update()
+{
+	// ESCAPE BUTTON
+	if (isKeyPressed(VK_ESCAPE))
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void Input::ChangeKey(unsigned int key, bool state)
+{
+	m_key[key] = state;
+}
+
+bool Input::isKeyPressed(unsigned int key)
+{
+	return (m_key[key]);
+}
