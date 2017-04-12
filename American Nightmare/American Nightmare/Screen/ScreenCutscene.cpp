04fe5b8a701e783ce2f1a059477ae8449a9e6af7
Fredholm <te11c.simon.fredholm@gmail.com>
2017-04-12 11:41:46 +0200

    Added to ScreenManager, can be changed to by pressing F4

--- American Nightmare/American Nightmare/Screen/ScreenCutscene.cpp
+++ American Nightmare/American Nightmare/Screen/ScreenCutscene.cpp
@@ -1,1 +1,45 @@
 #include "ScreenCutscene.h"
+
+ScreenCutscene::ScreenCutscene() : Screen() { }
+
+ScreenCutscene::ScreenCutscene(const ScreenCutscene& other) { }
+
+ScreenCutscene::~ScreenCutscene() { }
+
+bool ScreenCutscene::Start()
+{
+	Screen::Start();
+
+	// Initialize movie stuff
+
+	SetStartVariables();
+
+	return true;
+}
+
+void ScreenCutscene::Stop()
+{
+	Screen::Stop();
+
+	// Unload movie
+}
+
+bool ScreenCutscene::StartCutscene(std::string movieName)
+{
+	return false;
+}
+
+void ScreenCutscene::SetStartVariables()
+{
+	// Set variables
+}
+
+void ScreenCutscene::Update()
+{
+
+}
+
+void ScreenCutscene::Draw()
+{
+
+}
