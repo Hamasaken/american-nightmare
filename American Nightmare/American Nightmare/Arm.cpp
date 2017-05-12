1733e3253413afc88cc8e85d23a92ef16a94fed2
Lucas <Lucas@DESKTOP-5TCJFHJ>
2017-05-12 14:07:34 +0200

    Added arm Class

---
+++ American Nightmare/American Nightmare/Arm.cpp
@@ -0,0 +1,31 @@
+#include "Arm.h"
+
+Arm::Arm()
+{
+}
+
+Arm::~Arm()
+{
+}
+
+bool Arm::Start(std::string modelName, const MaterialManager::Material* material, b2World* world, b2Body* player)
+{
+	Entity::StartArm(modelName, material, world, player);
+
+	jointDef.bodyA = player;
+	jointDef.bodyB = hitbox->getBody();
+	jointDef.collideConnected = false;
+
+	jointDef.localAnchorA.Set(0, 1.66);
+	jointDef.localAnchorB.Set(-0.4, 0);
+
+	jointDef.referenceAngle = -90;
+
+	return true;
+}
+
+b2Body* Arm::getBody()
+{
+	return hitbox->getBody();
+
+}
