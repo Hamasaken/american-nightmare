01636a2cfe631b4ae060355d76c0d2ef9072f65e
Fredholm <te11c.simon.fredholm@gmail.com>
2017-05-16 16:38:25 +0200

    Forgot to remove temp map

--- American Nightmare/American Nightmare/Game/LevelManager.cpp
+++ American Nightmare/American Nightmare/Game/LevelManager.cpp
@@ -170,7 +170,7 @@ bool LevelManager::LoadLevel(GLuint shader, std::string levelPath, std::string a
 	//lightManager->AddDirectionalLight(glm::vec4(0, 20, 20, 1), glm::vec4(0.f, -0.5f, -1, 1), glm::vec4(1, 1, 1, 1), glm::vec4(1, 1, 1, 1), 0.1f);
 
 	// Loading temp level
-	LoadTempLevel(shader);
+//	LoadTempLevel(shader);
 
 	return true;
 }
