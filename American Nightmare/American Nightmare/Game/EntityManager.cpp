cfdacb99000954a4ce5fb01e7e659bc05a36c9ef
Fredholm <te11c.simon.fredholm@gmail.com>
2017-05-22 17:10:51 +0200

    support for dead enemies

--- American Nightmare/American Nightmare/Game/EntityManager.cpp
+++ American Nightmare/American Nightmare/Game/EntityManager.cpp
@@ -96,14 +96,12 @@ void EntityManager::Update(GLfloat deltaT, glm::vec3 playerPosition, bool player
 	{
 		Enemy* e = enemyList[i];
 		e->Update(deltaT, b2Vec2(playerPosition.x, playerPosition.y), playerDead);
-	/*	if (e->getIsDead())
+		if (e->get())
 		{
 			e->Stop();
 			delete e;
 			enemyList.erase(enemyList.begin() + i);
 		}
-
-		*/
 	}
 
 	for (Entity* e : entityList)
