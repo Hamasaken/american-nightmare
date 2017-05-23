3a0bf3af2d0733f3f2fdf2f3028a5004c00a5100
Emanuel <Emanuel@DESKTOP-NSEPAFP>
2017-05-23 17:00:19 +0200

    optimzation

--- American Nightmare/American Nightmare/MyContactListener.cpp
+++ American Nightmare/American Nightmare/MyContactListener.cpp
@@ -22,59 +22,66 @@ void MyContactListener::BeginContact(b2Contact* contact)
 	Object* bodyA = static_cast<Object*>(contact->GetFixtureA()->GetBody()->GetUserData());
 	Object* bodyB = static_cast<Object*>(contact->GetFixtureB()->GetBody()->GetUserData());
 
-	Player* player = dynamic_cast<Player*>(bodyA);
+	Player* player = nullptr;
+	Enemy* enemy = nullptr;
+	Projectile* projectileA = nullptr;
+	Projectile* projectileB = nullptr;
+
+
+	player = dynamic_cast<Player*>(bodyA);
 	if (player && !player->getIsDead())
 	{
-		Enemy* enemy = dynamic_cast<Enemy*>(bodyB);
+		enemy = dynamic_cast<Enemy*>(bodyB);
 		if (enemy)
 		{
 			player->setContactWithEnemy(enemy);
 		}
-
-		Projectile* myProjectile = dynamic_cast<Projectile*>(bodyB);
-		if (myProjectile)
+		projectileA = dynamic_cast<Projectile*>(bodyB);
+		if (projectileA)
 		{
 			if (player->addPlayerProjectiles() == true && sf::Mouse::isButtonPressed(sf::Mouse::Right) == true)
 			{
 				player->addNrOfProjectiles();
-				myProjectile->setmarked(true);
+				projectileA->setmarked(true);
 				soundManager->playSFXOverDrive(SoundManager::SFX_SUCTION, 30, 0.15f);
 			}
 		}
 	}
-
-	Enemy* enemy = dynamic_cast<Enemy*>(bodyA);
-	if (enemy)
+	else
 	{
-		Projectile* myProjectile = dynamic_cast<Projectile*>(bodyB);
-		if (myProjectile)
+		enemy = dynamic_cast<Enemy*>(bodyA);
+		if (enemy)
 		{
-			particleManager->EffectBloodSplatter(enemy->getPosition(), getAngleFromTwoPoints(bodyA->getCenter(), bodyB->getCenter()), 0.08f, 25, glm::vec4(0.67f, 0.1f, 0.05f, 1.f));
-			soundManager->playSFX(SoundManager::SFX_HIT);	// temp hit sfx
-			enemy->TakeDamage(enemy->getDamage());
-			if (enemy->getIsDead()) particleManager->EffectExplosionLights(enemy->getPosition(), 25, glm::vec4(0.67f, 0.1f, 0.05f, 1.f)); 
+			projectileA = dynamic_cast<Projectile*>(bodyB);
+			if (projectileA)
+			{
+				particleManager->EffectBloodSplatter(enemy->getPosition(), getAngleFromTwoPoints(bodyA->getCenter(), bodyB->getCenter()), 0.08f, 25, glm::vec4(0.67f, 0.1f, 0.05f, 1.f));
+				soundManager->playSFX(SoundManager::SFX_HIT);	// temp hit sfx
+				enemy->TakeDamage(enemy->getDamage());
+				if (enemy->getIsDead()) particleManager->EffectExplosionLights(enemy->getPosition(), 25, glm::vec4(0.67f, 0.1f, 0.05f, 1.f));
+			}
+		}
+		else
+		{
+			Vacuum* vacuumA = dynamic_cast<Vacuum*>(bodyA);
+			Vacuum* vacuumB = dynamic_cast<Vacuum*>(bodyB);
+			if (vacuumA || vacuumB)
+			{
+				//printf("vacuum IS ALIVE First\n");
+				projectileA = dynamic_cast<Projectile*>(bodyA);
+				projectileB = dynamic_cast<Projectile*>(bodyB);
+				if (projectileA)
+				{
+					projectileA->setIsInVacRange(true);
+				}
+				else if (projectileB)
+				{
+					projectileB->setIsInVacRange(true);
+				}
+			}
 		}
 	}
 
-		//Fixa Senare
-
-		Vacuum* vacuumA = dynamic_cast<Vacuum*>(bodyA);
-		Vacuum* vacuumB = dynamic_cast<Vacuum*>(bodyB);
-		if (vacuumA || vacuumB)
-		{
-			//printf("vacuum IS ALIVE First\n");
-			Projectile* projectileA = dynamic_cast<Projectile*>(bodyA);
-			Projectile* projectileB = dynamic_cast<Projectile*>(bodyB);
-			if (projectileA)
-			{
-				projectileA->setIsInVacRange(true);
-				
-			}
-			else if(projectileB)
-			{
-				projectileB->setIsInVacRange(true);
-			}
-		}
 
 }
 
