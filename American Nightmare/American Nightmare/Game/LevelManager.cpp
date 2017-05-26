79f34101c4ca36ccc1c1b0102c211ebea3fffd1b
Fredholm <te11c.simon.fredholm@gmail.com>
2017-05-26 13:51:27 +0200

    mem leaks on ph

--- American Nightmare/American Nightmare/Game/LevelManager.cpp
+++ American Nightmare/American Nightmare/Game/LevelManager.cpp
@@ -150,6 +150,13 @@ void LevelManager::Stop()
 		entityManager = nullptr;
 	}
 
+	if (myPH != nullptr)
+	{
+		myPH->deleteProjects(world);
+		delete myPH;
+		myPH = nullptr;
+	}
+
 	if (world != nullptr)
 	{
 		b2Body* body = world->GetBodyList();
