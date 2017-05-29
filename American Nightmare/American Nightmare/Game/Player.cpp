ae6f3c183d44fa5760fa2376002f9672c7689235
Grupp4 <Grupp4@DESKTOP-HD96879>
2017-05-29 17:41:36 +0200

    nothing

--- American Nightmare/American Nightmare/Game/Player.cpp
+++ American Nightmare/American Nightmare/Game/Player.cpp
@@ -455,7 +455,9 @@ void Player::InputController(GLint deltaT)
 
 		if (sf::Joystick::isButtonPressed(0, BTN_X) && power >= deltaT * 0.001 * PLAYER_POWER_COST_HOVER)
 		{
+			cout << "something special" << endl;
 			Hover(deltaT);
+			isHovering = true;
 		}
 		else isHovering = false;
 
